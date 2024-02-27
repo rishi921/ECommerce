08d5ea9c06e1796e75f2f707201c0de0a68c02e9
rishi921 <shrishidurga21@gmail.com>
2024-02-27 14:54:39 +0530

    second commit

--- src/Contact.js
+++ src/Contact.js
@@ -33,9 +33,59 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper>
-    <h1>Contact Page</h1>
-  </Wrapper>;
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.4847317077138!2d72.88639711194253!3d19.0863794820457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c887efb78b9f%3A0x9f9dc99c3119470a!2sPhoenix%20Marketcity!5e0!3m2!1sen!2sin!4v1709023739809!5m2!1sen!2sin"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xleqnldy"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+              // value=""
+            ></input>
+
+            <input
+              type="email"
+              placeholder="email"
+              name="Email"
+              required
+              autoComplete="off"
+              // value=""
+            ></input>
+
+            <textarea
+              name="Message"
+              placeholder="Enter Your Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+            ></textarea>
+            <input type="submit" value="send"></input>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Contact;
--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -96,7 +96,6 @@ ${"" /* resuable code section  */}
 
 .grid-two-column {
   grid-template-columns: repeat(2, 1fr);
-
 }
 
 .grid-three-column {
@@ -157,8 +156,7 @@ input, textarea{
     cursor: pointer;
     }
 
-${
-  "" /* @media (max-width: ${({ theme }) => theme.media.tab}) {
+@media (max-width: ${({ theme }) => theme.media.tab}) {
     .container {
     max-width: 130rem;
     padding: 0 3.2rem;
@@ -168,8 +166,7 @@ ${
    @media (max-width: ${({ theme }) => theme.media.mobile}) {
        html {
       font-size: 50%;
-    } */
-}
+    }
 
 .grid{
   gap: 3.2rem;
