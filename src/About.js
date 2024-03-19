dc16617278fbcb6d074a2a0269bf60657910b8a9
rishi921 <shrishidurga21@gmail.com>
2024-03-19 15:37:35 +0530

    updated

--- src/About.js
+++ src/About.js
@@ -1,12 +1,19 @@
-import React from "react";
 import HeroSection from "./components/HeroSection";
+import { useProductContext } from "./context/productcontext";
 
 const About = () => {
+  const myName = useProductContext();
+
   const data = {
     name: "Sharma's Ecommerce",
   };
 
-  return <HeroSection myData={data} />;
+  return (
+    <>
+      {myName}
+      <HeroSection myData={data} />{" "}
+    </>
+  );
 };
 
 export default About;
---
+++ src/context/productcontext.js
@@ -0,0 +1,22 @@
+// Create a context
+// provider
+// consumer => useContext Hook
+
+import { createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value={{ myName: "Rishi" }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom hooks
+const useProductContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider, AppContext, useProductContext };
